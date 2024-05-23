468e61529a462bb291f5760a8dea78ba3e2c014d
nahyan0077 <nahyanmp4@gmail.com>
2024-05-23 14:09:06 +0530

    fix: send grid cofig fixed

--- notification-service/src/infrastructure/routes/index.ts
+++ notification-service/src/infrastructure/routes/index.ts
@@ -1,6 +1,6 @@
-import { jwtMiddleware } from '@/_lib/common/middlewares/jwtMiddleware'
+import { jwtMiddleware } from '../../_lib/common/middlewares/jwtMiddleware'
 import { IDependencies } from '@/application/interfaces/IDependencies'
-import { controllers } from '@/presentation/controllers'
+import { controllers } from '../../presentation/controllers'
 import {Router} from 'express'
 
 export const notificationRouter = (dependencies: IDependencies) => {
