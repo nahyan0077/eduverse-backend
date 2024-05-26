c1ea347dd8471bf6759c2b57521e0650e027b690
nahyan0077 <nahyanmp4@gmail.com>
2024-05-26 22:31:56 +0530

    initial commit : user-services

---
+++ user-service/src/infrastructure/database/mongo/models/UserModel.ts
@@ -0,0 +1,90 @@
+import { UserEntity } from "@/domain/entities";
+import mongoose, { Schema, model } from "mongoose";
+
+
+const userSchema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			enum: ["student", "instructor", "admin","pending"],
+			default: "pending",
+		},
+		profile: {
+			avatar: {
+				type: String,
+				default: "https://www.pngkey.com/png/detail/72-729716_user-avatar-png-graphic-free-download-icon.png"
+			},
+			dataOfBirth: {
+				type: String,
+			},
+			gender: {
+				type: String,
+				enum: ["male", "female", "other"],
+			},
+		},
+		contact: {
+			address: {
+				type: String,
+			},
+			phone: {
+				type: String,
+			},
+			social: {
+				type: String
+			},
+		},
+		profession: {
+            enum: ["student", "working"],
+            type: String,
+		},
+		otp: {
+            type: String,
+		},
+		profit: {
+            type: Number,
+			default: 0,
+		},
+        isBlocked: {
+            type: Boolean,
+            default: false,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+		isGAuth: {
+			type: Boolean,
+			default: false
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+
+export const User = model<UserEntity>("users",userSchema)
