357b13d3012b3aed51222128cef6cde363158ecd
nahyan0077 <nahyanmp4@gmail.com>
2024-05-27 14:11:38 +0530

    fix: type in controller

--- user-service/src/presentation/controllers/index.ts
+++ user-service/src/presentation/controllers/index.ts
@@ -1,9 +1,10 @@
 import { IDependencies } from "@/application/interfaces/IDependencies";
 import { getAllInstructorsController } from "./getAllInstructorsController";
+import { getAllStudentsController } from "./getAllStudentsController";
 
 export const controllers = (dependencies: IDependencies) => {
     return {
         getAllInstructors: getAllInstructorsController(dependencies),
-        getAllStudents: getAllInstructorsController(dependencies)
+        getAllStudents: getAllStudentsController(dependencies)
     }
 }
