2e1e272cad635e418655db7f99397be715bf2561
nahyan0077 <nahyanmp4@gmail.com>
2024-06-04 10:06:29 +0530

    refactor: add jwt middleware for token refreshing

--- user-service/src/infrastructure/routes/index.ts
+++ user-service/src/infrastructure/routes/index.ts
@@ -1,6 +1,7 @@
 import { IDependencies } from "@/application/interfaces/IDependencies";
 import { controllers } from "../../presentation/controllers";
 import { Router } from "express";
+import { jwtMiddleware } from "../../_lib/common/middlewares/jwtMiddleware";
 
 export const userRoutes = (dependencies: IDependencies) => {
 	const {
@@ -13,11 +14,15 @@ export const userRoutes = (dependencies: IDependencies) => {
 
 	const router = Router();
 
-	router.route("/get-all-instructors").get(getAllInstructors);
-	router.route("/get-all-students").get(getAllStudents);
-	router.route("/admin-block-unblock").patch(blockUnblockUser);
-	router.route("/verify-instructor").patch(verifyInstructor);
-	router.route("/reject-instructor").patch(rejectInstructor);
+	router.route("/get-all-instructors").get(jwtMiddleware,getAllInstructors);
+
+	router.route("/get-all-students").get(jwtMiddleware,getAllStudents);
+
+	router.route("/admin-block-unblock").patch(jwtMiddleware,blockUnblockUser);
+
+	router.route("/verify-instructor").patch(jwtMiddleware,verifyInstructor);
+
+	router.route("/reject-instructor").patch(jwtMiddleware,rejectInstructor);
 
 	return router;
 };
