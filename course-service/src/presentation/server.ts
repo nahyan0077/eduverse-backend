c13239621d5c5674b81798cebb72a31d6ca84cfb
nahyan0077 <nahyanmp4@gmail.com>
2024-06-05 01:31:54 +0530

    initial commit in course services

---
+++ course-service/src/presentation/server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction, Application } from 'express'
+import cookieParser from 'cookie-parser'
+import {config} from 'dotenv'
+
+config()
+
+
+const app: Application = express()
+
+const PORT: number = Number(process.env.PORT) || 7001
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: `${process.env.SERVICE} ON!`
+    })
+});
+
+
+app.use("*",(req: Request, res: Response) => {
+    res.status(404).json({ success: false, status: 404, message: "Api Not found" });
+  });  
+
+
+  
+const start = () => {
+    app.listen(PORT, () => {
+        console.log(`The ${process.env.SERVICE} is listening on port ${PORT}`);
+    })
+}
+
+export default { start }
