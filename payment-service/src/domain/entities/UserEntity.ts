cbc2b24996b34f76a353b0073e1efd307aa48e27
nahyan0077 <nahyanmp4@gmail.com>
2024-06-12 15:04:34 +0530

    intitial commit payment service

---
+++ payment-service/src/domain/entities/UserEntity.ts
@@ -0,0 +1,48 @@
+
+enum Role {
+    pending='pending',
+    student='student',
+    teacher='teacher',
+    admin='admin'
+}
+
+enum Gender {
+    male = 'male',
+    female = 'female',
+    other = 'other'
+}
+
+enum Profession {
+    student = 'student',
+    working = 'working'
+}
+
+interface Contact {
+    phone?: string,
+    social?: string,
+    address: string
+}
+
+interface Profile {
+    avatar: string,
+    dateOfBirth?: string,
+    gender: string
+}
+
+
+export interface UserEntity {
+    first_Name: string,
+    last_Name: string,
+    userName: string,
+    email: string,
+    password: string,
+    role: Role,
+    contact: Contact,
+    profile: Profile,
+    otp: string,
+    profession: Profession,
+    isBlocked: boolean;
+    isVerified: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
