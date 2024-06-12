d42299c6400c52c39b37f085caf6bbc028f00d35
nahyan0077 <nahyanmp4@gmail.com>
2024-06-12 17:41:12 +0530

    feat: payment features added

---
+++ payment-service/src/_lib/http/jwt/generateAccessToken.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+    _id: string;
+    email: string;
+    role: string;
+}
+
+export const generateAccessToken = (
+    payload: UserPayload
+) => {
+
+    const { _id, email, role } = payload;
+    const newPayload = { _id, email, role };
+
+    return jwt.sign(
+        newPayload,
+        String(process.env.ACCESS_TOKEN_SECRET),
+        { expiresIn: '30m' }  
+    );
+};
