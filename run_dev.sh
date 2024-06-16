5ba75ecf2eee4a01bf37b73f8b6acadbb27ccb8c
nahyan0077 <nahyanmp4@gmail.com>
2024-06-16 22:59:21 +0530

    feat: auto run services file addded

---
+++ run_dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Run api-gateway
+cd api-gateway/
+npm run dev &
+
+# Run auth-service
+cd ../auth-service/
+npm run dev &
+
+# Run user-service
+cd ../user-service/
+npm run dev &
+
+# Run course-service
+cd ../course-service/
+npm run dev &
+
+# Run notification-service
+cd ../notification-service/
+npm run dev &
+
+# Run payment-service
+cd ../payment-service/
+npm run dev &
+
+wait
