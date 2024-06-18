01a69f04a78fda82d224084edea572e08e2b0e99
nahyan0077 <nahyanmp4@gmail.com>
2024-06-18 20:57:59 +0530

    create payment controller setup initiated

---
+++ payment-service/src/presentation/controller/createPayment.ts
@@ -0,0 +1,22 @@
+import { IDependencies } from "@/application/interfaces/IDependencies";
+import { Request, Response, NextFunction } from "express";
+
+
+export const createPaymentSessionController = (dependencies: IDependencies) => {
+    const { useCases: {createPaymentUseCase} } = dependencies
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            
+            const { instructorId, ...data } = req.body;
+
+            const result = await createPaymentUseCase(dependencies).execute(req.body)
+
+            if (!result) {
+                throw new Error("payment failed!");
+            }
+
+        } catch (error: any) {
+            
+        }
+    }
+}
