52ef40012e050af6d8c67ccec72ce0b319635fa7
nahyan0077 <nahyanmp4@gmail.com>
2024-06-19 11:30:19 +0530

    create enrollment controller initiated

---
+++ course-service/src/presentation/controller/enrollment/createEnrollment.ts
@@ -0,0 +1,6 @@
+import { IDependencies } from "@/application/interfaces/IDependencies";
+
+export const createEnrollment = (dependencies: IDependencies) => {
+    const { useCases: {createEnrollmentUseCase} } = dependencies
+    
+}
