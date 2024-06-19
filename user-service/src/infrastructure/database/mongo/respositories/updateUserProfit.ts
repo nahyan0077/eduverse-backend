f7d104f1039883753b0c65eee6b313693bca5f82
nahyan0077 <nahyanmp4@gmail.com>
2024-06-19 10:09:03 +0530

    refact: create payment repository added

---
+++ user-service/src/infrastructure/database/mongo/respositories/updateUserProfit.ts
@@ -0,0 +1,29 @@
+import { UserEntity } from "@/domain/entities";
+import { User } from "../models";
+
+export const updateUserProfit = async (
+	userId: string,
+	amount: number
+): Promise<UserEntity | null> => {
+	try {
+		const instructorProfit = amount * 0.7;
+		const adminProfit = amount * 0.3;
+
+		const updateInstructorProfit = await User.findByIdAndUpdate(
+			userId,
+			{ $inc: { profit: instructorProfit } },
+			{ new: true }
+		);
+
+		const updateAdminProfit = await User.findOneAndUpdate(
+			{ role: "admin" },
+			{ $inc: { profit: adminProfit } },
+			{ new: true }
+		);
+
+		return updateInstructorProfit;
+	} catch (error: any) {
+		console.error("Error updating user profit:", error);
+		throw new Error(error?.message);
+	}
+};
