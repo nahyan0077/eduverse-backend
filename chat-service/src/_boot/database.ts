6f45b380196d840ec52cb68648085c96975bd77b
nahyan0077 <nahyanmp4@gmail.com>
2024-06-23 23:28:23 +0530

    initial commit in chat-service

---
+++ chat-service/src/_boot/database.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
+export default async () => {
+	try {
+		const mongoUrl = process.env.MONGO_URI;
+
+		if (!mongoUrl) {
+			throw new Error(
+				"MongoDB connection string not provided in environment variables"
+			);
+		}
+
+		await mongoose.connect(mongoUrl.trim());
+		console.log(`🍃 MongoDB connected successfully ---> ${process.env.SERVICE} 🍃`);
+	} catch (error: any) {
+		console.error(`❌ Database Connection failed❌`);
+		console.error(error.message);
+		process.exit(1);
+	}
+};
