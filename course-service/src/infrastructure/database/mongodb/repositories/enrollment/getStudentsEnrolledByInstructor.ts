b5545111218d755cbf7ccd65062b1b167ed6cd64
nahyan0077 <nahyanmp4@gmail.com>
2024-06-25 14:32:35 +0530

    refact: updated the repository data

--- course-service/src/infrastructure/database/mongodb/repositories/enrollment/getStudentsEnrolledByInstructor.ts
+++ course-service/src/infrastructure/database/mongodb/repositories/enrollment/getStudentsEnrolledByInstructor.ts
@@ -7,9 +7,12 @@ export const getStudentsEnrolledByInstructor = async (instructorId: string) => {
 
 		const courseIds = courses.map((course) => course._id);
 
-		const enrollments = await Enrollment.find({ courseId: { $in: courseIds } }).populate('userId')
+		const enrollments = await Enrollment.find({
+			courseId: { $in: courseIds },
+		}).populate("userId");
 
 		return enrollments;
+        
 	} catch (error: any) {
 		throw ErrorResponse.internalError(error?.message);
 	}
