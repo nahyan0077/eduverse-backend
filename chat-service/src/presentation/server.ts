3ddc317bcdd1c77ca903a215aa3e7ce973d8ae6a
nahyan0077 <nahyanmp4@gmail.com>
2024-07-15 20:56:12 +0530

    updated manifisests and ingress

--- chat-service/src/presentation/server.ts
+++ chat-service/src/presentation/server.ts
@@ -1,34 +1,21 @@
-import express, {
-  Request,
-  Response,
-  Application,
-  NextFunction,
-  ErrorRequestHandler,
-} from "express";
+import express, { Request, Response, Application } from "express";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import morgan from "morgan";
 import { routes } from "../infrastructure/routes";
 import { dependancies } from "../_boot/dependancies";
 import errorHandler from "../_lib/common/error/errorhandler";
-import http from "http";
-import { socket } from "@/_boot/socket";
 import cors from "cors";
 
 config();
 
 const app: Application = express();
-const PORT: number = Number(process.env.PORT) || 4001;
 
 // Middleware
 app.use(express.json());
-// app.use()
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use(morgan("dev"));
-
-// app.use(cors,)
 
 app.use(
   cors({
@@ -38,10 +25,6 @@ app.use(
   })
 );
 
-// const server = http.createServer(app);
-
-// socket(server);
-// test route
 app.get("/api/chat/test", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Chat service ON!",
@@ -58,11 +41,5 @@ app.all("*", (req: Request, res: Response) => {
 });
 
 app.use(errorHandler);
-
-// server.listen(process.env.PORT, () => {
-//   console.log(
-//     `The ${process.env.SERVICE} is listening on port ${process.env.PORT}`
-//   );
-// });
 
 export default app;
