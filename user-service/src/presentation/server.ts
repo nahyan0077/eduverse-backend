f93bceaa9ce7422f47435f327c8da7304299622a
nahyan0077 <nahyanmp4@gmail.com>
2024-08-16 14:33:33 +0530

    feat: cors updated on user services

--- user-service/src/presentation/server.ts
+++ user-service/src/presentation/server.ts
@@ -5,18 +5,30 @@ import morgan from "morgan";
 import { userRoutes } from "../infrastructure/routes";
 import { dependencies } from "../_boot/dependencies";
 import helmet from 'helmet'
+import cors from 'cors'
 
-config();
+// Load environment-specific variables
+if (process.env.NODE_ENV === "production") {
+  config({ path: "./.env.production" });
+} else {
+  config({ path: "./.env.local" });
+}
 
 const app: Application = express();
-
 const PORT: number = Number(process.env.PORT) || 6001;
+
+const corsOptions = {
+  origin: String(process.env.FRONTEND_URL),
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(helmet())
+app.use(cors(corsOptions))
 
 //test route
 app.get("/api/user/test", (req: Request, res: Response) => {
