4bad2a062142c32ae2af1cae4fbf5f79f1669c0c
nahyan0077 <nahyanmp4@gmail.com>
2024-08-24 15:06:09 +0530

    updated logout cookies

--- auth-service/src/presentation/controller/logout.ts
+++ auth-service/src/presentation/controller/logout.ts
@@ -4,8 +4,16 @@ import { Request, Response, NextFunction } from "express";
 export const logoutController = (dependancies: IDependancies) => {
 	return async (req: Request, res: Response, next: NextFunction) => {
 		try {
-			res.cookie("access_token", "", { maxAge: 1 });
-			res.cookie("refresh_token", "", { maxAge: 1 });
+
+			const cookieOptions : any = {
+                httpOnly: true,
+                secure: true,
+                sameSite: "none",
+                maxAge: 0
+            };
+
+			res.cookie("access_token", "", cookieOptions);
+			res.cookie("refresh_token", "", cookieOptions);
 			res.status(204).json({});
 		} catch (error: any) {
 			next(error);
